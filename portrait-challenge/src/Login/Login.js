7951b4695dfe8b3d7afc79ebc6c4c6aff291ff7f
Maxi Roldán <mroldan@bitbucket.com>
2020-09-27 21:21:15 -0300

    Add inputs and buttons to Login component

--- portrait-challenge/src/Login/Login.js
+++ portrait-challenge/src/Login/Login.js
@@ -11,6 +11,27 @@ function Login() {
                     </div>
                 </div>
             </div>
+            <div className="row">
+                <div className="col-12">
+                    <div className="login-input-group">
+                        <input className="login-input" type="password" placeholder="Your password"/>
+                    </div>
+                </div>
+            </div>
+            <div className="row ">
+                <div className="col-12 d-flex justify-content-center">
+                    <div className="login-sing-in-form">
+                        <button className="login-sing-in-button">Sign in</button>
+                    </div>
+                </div>  
+            </div>
+            <div className="row">
+                <div className="col-12 d-flex justify-content-center">
+                    <div className="login-new-account-form">
+                        <a className="login-new-account-button">New Account</a>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
